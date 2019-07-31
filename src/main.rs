094699baee5deccbf9703c7c7d56ec367ed1f707
Ryan Kurte <ryankurte@gmail.com>
2019-07-31 15:40:11 +1200

    Initial project setup

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+extern crate embedded_hal;
+extern crate linux_embedded_hal;
+
+extern crate structopt;
+
+extern crate log;
+extern crate simplelog;
+
+fn main() {
+    println!("Hello, world!");
+}
